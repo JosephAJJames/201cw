dff486d020c8d95523a8b05a411abc24408ec738
jamesj9 <jamesj9@lancaster.ac.uk>
2024-03-02 18:55:05 +0000

    New csv file

--- src/Code.java
+++ src/Code.java
@@ -1,11 +1,17 @@
+import java.io.FileReader;
 import java.sql.*;
+import java.io.BufferedReader;
 
 public class Code {
         public static void main(String[] args) throws SQLException, ClassNotFoundException {
         System.out.println("Trying to connect....");
         String url = "jdbc:mysql://localhost:3306/";
 
+
+        csvReader reader = new csvReader("38639416");
+
         Connection con = connect(url);
+
     }
 
     public static Connection connect(String url) throws SQLException {
@@ -16,5 +22,15 @@ public class Code {
                 System.out.println("Failed to connect...:\n"+ e);
             }
             return null;
+    }
+
+}
+
+class csvReader {
+    BufferedReader bufferedReader;
+
+    public csvReader(String fileName)
+    {
+        bufferedReader = new BufferedReader(new FileReader());
     }
 }
