bcba862155f2384f21a5777370d24226a53a593c
JosephAJJames <130866326+JosephAJJames@users.noreply.github.com>
2024-03-05 17:31:11 +0000

    Update Code.java

--- src/Code.java
+++ src/Code.java
@@ -7,26 +7,26 @@ import java.util.List;
 
 public class Code {
 
-    String[] joesSQLStatments = {"DROP DATABASE PremGames;" , "CREATE DATABASE PremGames;", "USE PremGames;",
+    String[] joesSQLStatments = {"DROP DATABASE IF EXISTS PremGames" ,"CREATE DATABASE PremGames;", "USE PremGames;",
 
             "CREATE TABLE Coaches(cID INT PRIMARY KEY NOT NULL, FirstName VARCHAR(25), LastName VARCHAR(25), NumberOfTrophies INT, HasTeam BOOLEAN)",
 
             "CREATE TABLE Tactics(tID INT PRIMARY KEY NOT NULL,cID INT , PosStyle VARCHAR(10), Formation VARCHAR(10), TacticName VARCHAR(25), FOREIGN KEY (cID) REFERENCES Coaches(cID));",
 
-            "CREATE TABLE Referees(rID INT PRIMARY KEY NOT NULL, FirstName VARCHAR(25), LastName VARCHAR(25), yCardsThisYear INT, rCardsThisYear INT)",
-
-            "CREATE TABLE Fixtures(fID INT PRIMARY KEY NOT NULL, KickOff DATETIME, WeatherConditions VARCHAR (25), HomeScore INT, AwayScore INT, rID INT, FOREIGN KEY rID REFERENCES Referees(rID), tmID Integer NOT NULL UNIQUE, FOREIGN KEY tmID REFERENCES TeamMatchups(tmID))",   //referee would be NOT NULL however, referees for future games have not yet been decided
-
             "CREATE TABLE Teams(tID INT PRIMARY KEY NOT NULL, TeamName VARCHAR(25), Region VARCHAR(25), YearFounded INT, PlaysInUCL BOOLEAN, cID INT UNIQUE, Owner VARCHAR(30))",
+
+            "CREATE TABLE Referees(rID INT PRIMARY KEY NOT NULL, FirstName VARCHAR(25), LastName VARCHAR(25), yCardsThisYear INT, rCardsThisYear INT)",
 
             "CREATE TABLE TeamMatchups(tmID INT PRIMARY KEY NOT NULL, HomeTeam INT NOT NULL, AwayTeam INT NOT NULL, FOREIGN KEY HomeTeam REFERENCES Teams(tID), FOREIGN KEY AwayTeam REFERENCES Teams(tID));",
 
+            "CREATE TABLE Fixtures(fID INT PRIMARY KEY NOT NULL, KickOff DATETIME, WeatherConditions VARCHAR (25), HomeScore INT, AwayScore INT, rID INT, FOREIGN KEY rID REFERENCES Referees(rID), tmID Integer NOT NULL UNIQUE, FOREIGN KEY tmID REFERENCES TeamMatchups(tmID))",   //referee would be NOT NULL however, referees for future games have not yet been decided
+
             "CREATE TABLE Players(pID INT PRIMARY KEY NOT NULL, tID INT, Position VARCHAR(5),DOB (DATE) , HeightCM INT), ShirtNum INT, FOREIGN KEY tID REFERENCES Teams(tID);",
 
-            "DROP DATABSE PremGames"};
+            "DROP DATABASE PremGames"};
 
-    String[] dannysSqlStatments = {"DROP DATABASE PremBusiness" ,"CREATE DATABASE PremBusiness;", "USE PremBusiness;", "DROP DATABASE PremBusiness"};
-
+    String[] dannysSqlStatments = {};
+    //"DROP DATABASE PremBusiness" ,"CREATE DATABASE PremBusiness;", "USE PremBusiness;", "DROP DATABASE PremBusiness"
 
     String[] scottsSQLStatments = {};
     public static void main(String[] args) throws SQLException {
